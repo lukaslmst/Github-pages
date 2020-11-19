3a50c34fb5f6876ef1cebac0d53a603aca506b37
lukaslmst <71823867+lukaslmst@users.noreply.github.com>
2020-11-19 11:59:21 +0100

    Add files via upload

---
+++ Github pages/app.js
@@ -0,0 +1,31 @@
+function myFunction(){
+
+document.getElementById('start').style.display="none";
+
+  
+
+  var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+  slideshows.forEach(initSlideShow);
+
+  function initSlideShow(slideshow) {
+
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+    var index = 0, time = 5000;
+    slides[index].classList.add('active');  
+    
+    setInterval( () => {
+      slides[index].classList.remove('active');
+      
+      index++;
+      
+      
+      if (index === slides.length) index = 0; 
+      
+      slides[index].classList.add('active');
+
+    }, time);
+  }
+
+}
